39cfa4c8fae3d8630df933ff13a7f5e84d581218
KSkryabin <Kirill.Skryabin@envision.design>
2020-11-19 11:09:29 +0200

    ToolbarModule has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MaterialModule } from './material-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatNativeDateModule } from '@angular/material/core';
+import { ToolbarModule } from './toolbar/toolbar.module';
 
 @NgModule({
     declarations: [AppComponent],
@@ -22,6 +23,7 @@ import { MatNativeDateModule } from '@angular/material/core';
         MaterialModule,
         MatNativeDateModule,
         ReactiveFormsModule,
+        ToolbarModule,
     ],
     providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }],
     bootstrap: [AppComponent],
