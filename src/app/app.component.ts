a9d209e80e56e170c69d7125e236caaf93962500
KSkryabin <Kirill.Skryabin@envision.design>
2020-11-24 11:21:31 +0200

    internationalization module has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './animations';
-import { TranslateService } from '@ngx-translate/core';
-import { environment } from '../environments/environment';
 
 @Component({
     selector: 'app-root',
@@ -10,34 +8,10 @@ import { environment } from '../environments/environment';
     styleUrls: ['./app.component.css'],
     animations: [slideInAnimation],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'olrxApp';
 
-    selectedLanguage: string | undefined;
-    languages: { id: string; title: string }[] = [];
-
-    constructor(private translateService: TranslateService) {}
-
-    ngOnInit(): void {
-        this.translateService.use(environment.defaultLocale);
-        this.selectedLanguage = environment.defaultLocale;
-
-        this.translateService
-            .get(environment.locales.map((x) => `LANGUAGES.${x.toUpperCase()}`))
-            .subscribe((translations) => {
-                // init dropdown list with TRANSLATED list of languages from config
-                this.languages = environment.locales.map((x) => {
-                    return {
-                        id: x,
-                        title: translations[`LANGUAGES.${x.toUpperCase()}`],
-                    };
-                });
-            });
-    }
-
-    changeLocale(): void {
-        this.translateService.use(this.selectedLanguage as string);
-    }
+    constructor() {}
 
     getAnimationData(outlet: RouterOutlet): any {
         return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
--- src/app/main-content/posters/posters-list/poster/poster.module.ts
+++ src/app/main-content/posters/posters-list/poster/poster.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PosterComponent } from './poster.component';
 import { MaterialModule } from 'src/app/material-module';
+import { InternationalizationModule } from 'src/app/internationalization.module';
 
 @NgModule({
     declarations: [PosterComponent],
-    imports: [CommonModule, MaterialModule],
+    imports: [CommonModule, MaterialModule, InternationalizationModule],
     exports: [PosterComponent],
 })
 export class PosterModule {}
