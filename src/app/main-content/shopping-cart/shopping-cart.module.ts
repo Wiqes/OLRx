fa1c99406cb3da97732e8b0b30a60b542d2c2122
KSkryabin <Kirill.Skryabin@envision.design>
2020-11-27 18:04:18 +0200

    the table has been added

--- src/app/main-content/shopping-cart/shopping-cart.module.ts
+++ src/app/main-content/shopping-cart/shopping-cart.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingCartComponent } from './shopping-cart.component';
+import { MaterialModule } from 'src/app/material-module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
     declarations: [ShoppingCartComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MaterialModule, MatTableModule],
     exports: [ShoppingCartComponent],
 })
 export class ShoppingCartModule {}
