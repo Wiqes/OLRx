f7015bb235089331b625e1e56a57144c684fd866
KSkryabin <Kirill.Skryabin@envision.design>
2020-12-18 19:02:52 +0200

    save

---
+++ src/app/ui-components/button-with-delay/button-with-delay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-with-delay',
+  templateUrl: './button-with-delay.component.html',
+  styleUrls: ['./button-with-delay.component.scss']
+})
+export class ButtonWithDelayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
