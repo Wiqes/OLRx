34a2d964611303b1606caf0eb0588cf81e340e36
KSkryabin <Kirill.Skryabin@envision.design>
2020-12-21 14:37:15 +0200

    save

--- src/app/ui-components/button-with-delay/button-with-delay.component.ts
+++ src/app/ui-components/button-with-delay/button-with-delay.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { delay, takeUntil } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
-  selector: 'app-button-with-delay',
-  templateUrl: './button-with-delay.component.html',
-  styleUrls: ['./button-with-delay.component.scss']
+    selector: 'app-button-with-delay',
+    templateUrl: './button-with-delay.component.html',
+    styleUrls: ['./button-with-delay.component.scss'],
 })
 export class ButtonWithDelayComponent implements OnInit {
+    constructor(@Inject(DOCUMENT) private document: Document) {}
 
-  constructor() { }
+    @Input() text = 'Button';
+    @Input() type = 'button';
+    @Input() color: ThemePalette = 'primary';
 
-  ngOnInit(): void {
-  }
+    public waiting = false;
+    private calls = new Subject();
 
+    ngOnInit(): void {}
+
+    invokeDelay(): Observable<string> {
+        return of('').pipe(delay(2000));
+    }
+
+    onButtonClick(event: MouseEvent): void {
+        if (!this.waiting) {
+            console.log('dfdfdf');
+        }
+        this.waiting = true;
+        const buttonElement: HTMLElement = this.document.getElementById('butt') as HTMLElement;
+        event.stopPropagation();
+        buttonElement.click();
+        this.calls.next(true);
+        this.invokeDelay()
+            .pipe(takeUntil(this.calls))
+            .subscribe(() => {
+                this.waiting = false;
+            });
+    }
 }
