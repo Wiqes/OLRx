f130ce20c2e3463d9f3e3caa3c37790b23ceccab
KSkryabin <Kirill.Skryabin@envision.design>
2020-12-28 17:41:20 +0200

    save

--- src/services/api/shopping-cart.service.ts
+++ src/services/api/shopping-cart.service.ts
@@ -4,25 +4,17 @@ import { Store } from '@ngrx/store';
 import { Poster } from 'src/interfaces/poster.interface';
 import { GetShoppingCartPosters, RemoveShoppingCartPoster } from 'src/store/actions/shopping-cart.actions';
 import { ShoppingCartPostersState } from 'src/store/reducers/shopping-cart.reducer';
-import { AuthenticationService } from '../authentication.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ShoppingCartService {
-    constructor(
-        private store$: Store<ShoppingCartPostersState>,
-        private http: HttpClient,
-        private authService: AuthenticationService,
-    ) {
-        this.authService.getUsername().subscribe((username) => (this.username = username));
-    }
+    constructor(private store$: Store<ShoppingCartPostersState>, private http: HttpClient) {}
 
     private shoppingCartUrl = 'http://localhost:3000/shopping-cart';
-    private username = '';
 
     getPosters(): void {
-        this.http.get(`${this.shoppingCartUrl}/${this.username}`).subscribe((posters: any) => {
+        this.http.get(`${this.shoppingCartUrl}`).subscribe((posters: any) => {
             this.store$.dispatch(
                 new GetShoppingCartPosters({ posters: posters.map((poster: any) => new Poster(poster)) }),
             );
@@ -31,14 +23,10 @@ export class ShoppingCartService {
 
     removePoster(posterId: string): void {
         this.store$.dispatch(new RemoveShoppingCartPoster({ posterId }));
-        this.http
-            .put(`${this.shoppingCartUrl}/remove`, { username: this.username, posterId })
-            .subscribe((response) => response);
+        this.http.put(`${this.shoppingCartUrl}/remove`, { posterId }).subscribe((response) => response);
     }
 
     addPoster(posterId?: string): void {
-        this.http
-            .put(`${this.shoppingCartUrl}/add`, { username: this.username, posterId })
-            .subscribe((response) => response);
+        this.http.put(`${this.shoppingCartUrl}/add`, { posterId }).subscribe((response) => response);
     }
 }
